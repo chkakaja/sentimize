1c05d86590f41e71b46fab5f0751cfefe5891e39
Jack Zhang <15jzhang@gmail.com>
2016-05-26 12:19:48 -0700

    Setup client-side react framework, add index.jsx, render first App component

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+     <div className='test'>
+       <h1>Heloo wooorld</h1>
+     </div> 
+    )
+  }
+}
