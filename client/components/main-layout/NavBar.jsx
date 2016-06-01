aac6854f733bf97109d23c6c8846da4ee7fb68a0
Jack Zhang <15jzhang@gmail.com>
2016-05-31 18:40:34 -0700

    Add application main layout UI header with base styles

--- client/components/main-layout/NavBar.jsx
+++ client/components/main-layout/NavBar.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Link } from 'react-router';
 
-export default (
-  <div className="nav-bar">
-    <h1 className="nav-bar-title"><Link to="/">Sentimize</Link></h1>
-    <Link to="/record">Record</Link>
-    <Link to="/sessions">Sessions</Link>
-  </div>
-)
+export default class NavBar extends React.Component { 
+  render() {
+    return (
+      <div className="header">
+        
+        <div className="pure-menu pure-menu-horizontal pure-menu-fixed">
+          <ul className="pure-menu-list">
+            <li className="pure-menu-item"><Link to="/record" className="pure-menu-link">Record</Link></li>
+            <li className="pure-menu-item"><Link to="/sessions" className="pure-menu-link">Sessions</Link></li>
+            <li className="pure-menu-item"><a href="/logout" className="pure-menu-link">Log out</a></li>
+          </ul>
+        </div>
+
+        <div className="pure-menu-heading">
+          <Link className="home-link" to="/">Sentimize</Link>
+        </div>
+      </div>
+    )
+  }
+}
