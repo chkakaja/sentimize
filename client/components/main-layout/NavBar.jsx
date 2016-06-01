5ba743d88d01edb61e35e27c8b3d8db0e7425d9a
Jack Zhang <15jzhang@gmail.com>
2016-05-31 19:01:28 -0700

    Add footer to base style

--- client/components/main-layout/NavBar.jsx
+++ client/components/main-layout/NavBar.jsx
@@ -6,6 +6,9 @@ export default class NavBar extends React.Component {
   render() {
     return (
       <div className="header">
+        <div className="pure-menu-heading">
+          <Link className="home-link" to="/">Sentimize</Link>
+        </div>
         
         <div className="pure-menu pure-menu-horizontal pure-menu-fixed">
           <ul className="pure-menu-list">
@@ -13,10 +16,6 @@ export default class NavBar extends React.Component {
             <li className="pure-menu-item"><Link to="/sessions" className="pure-menu-link">Sessions</Link></li>
             <li className="pure-menu-item"><a href="/logout" className="pure-menu-link">Log out</a></li>
           </ul>
-        </div>
-
-        <div className="pure-menu-heading">
-          <Link className="home-link" to="/">Sentimize</Link>
         </div>
       </div>
     )
