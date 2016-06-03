b18733d07466692b41eace0700b30ebee94d8535
Katherine Hao <katherine.sj@gmail.com>
2016-06-02 19:03:50 -0700

    moved req handler to controller

--- server/controllers/SessionController.js
+++ server/controllers/SessionController.js
@@ -2,15 +2,15 @@ var Session = require('../models/SessionModel.js');
 
 module.exports = {
   createSession: function(req, res) {
-     var sessionObj = {}
+     var sessionObj = {
+      userId: req.user.id 
+     }
 
+     console.log(req.user.id, "REQ user Id")
     //need to get current user
       //maybe in req.body (if we sent it along with the post req)
     //make sesssionObj with matching foreign key of userId
-    return new Session(sessionObj)
-    .fetch({
-      withRelated: ['user']
-    }).save()
+    return new Session(sessionObj).save()
       .then(function(newSession) {
         console.log("coming back from saving in DB", newSession)
         res.status(302).send(newSession)
