b40f07d0dcb478dd38c9143994a41c7bb7893e14
Jack Zhang <15jzhang@gmail.com>
2016-06-04 10:38:36 -0700

    Add dummy data import

--- client/components/sessions-view/SessionsView.jsx
+++ client/components/sessions-view/SessionsView.jsx
@@ -3,11 +3,13 @@ import SessionEntry from './SessionEntry.jsx';
 import { browserHistory } from 'react-router';
 import $ from 'jquery';
 
+// import dummyData from './../../../data/session-data.json';
+
 export default class SessionsView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sessionEntries: []
+      sessionEntries: dummyData
     }
   }
 
