28e84bea2c1f742dcb17ec0fa46c5d07346df7e1
Christian Haug <csh@christianhaug.com>
2016-06-04 15:02:23 -0700

    Change general background style to black

--- client/components/main-layout/NavBar.jsx
+++ client/components/main-layout/NavBar.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Link } from 'react-router';
 
-export default class NavBar extends React.Component { 
+export default class NavBar extends React.Component {
   render() {
     return (
       <div className="nav-bar">
         <div className="pure-menu-heading">
           <Link className="home-link" to="/">Sentimize</Link>
         </div>
-        
+
         <div className="pure-menu pure-menu-horizontal pure-menu-fixed">
           <ul className="pure-menu-list">
             <li className="pure-menu-item"><Link to="/record" className="pure-menu-link">Record</Link></li>
             <li className="pure-menu-item"><Link to="/sessions" className="pure-menu-link">Sessions</Link></li>
-            <li className="pure-menu-item"><Link to="/reports" className="pure-menu-link">Reports</Link></li>
             <li className="pure-menu-item"><a href="/logout" className="pure-menu-link">Log out</a></li>
           </ul>
         </div>
