e7be9e1ae10ef6dd139107c2fbeaec572d714dd2
Jack Zhang <15jzhang@gmail.com>
2016-06-03 18:59:11 -0700

    Move dummy data to json file within new top level 'data' directory

--- client/components/sessions-view/SessionsView.jsx
+++ client/components/sessions-view/SessionsView.jsx
@@ -1,52 +1,13 @@
 import React from 'react';
 import SessionEntry from './SessionEntry.jsx';
 
+import sessionDummyData from './../../../data/session-data.json';
+
 export default class SessionsView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      testEntries: [
-        {
-          id: 25,
-          title: 'Pikachu',
-          subject: 'Jack Zhang',
-          description: 'A short description describing this session',
-          date: 'June 15, 2016',
-          duration: '0:03:58'
-        },
-        {
-          id: 3,
-          title: 'Longer Title',
-          subject: 'Chris Haug',
-          description: 'Short content',
-          date: 'June 15, 2016',
-          duration: '0:00:25'
-        },
-        {
-          id: 17,
-          title: 'My recording session!',
-          subject: 'Marcus Phillips',
-          description: 'A long description describing this session A long description describing this session',
-          date: 'June 15, 2016',
-          duration: '0:00:25'
-        },
-        {
-          id: 20,
-          title: 'Hack Reactor Interview',
-          subject: 'A person',
-          description: 'A short description describing this session',
-          date: 'June 15, 2016',
-          duration: '0:03:58'
-        },
-        {
-          id: 14,
-          title: 'Test',
-          subject: 'Kani Muni',
-          description: 'Some content',
-          date: 'June 15, 2016',
-          duration: '17:44:06'
-        }
-      ]
+      testEntries: sessionDummyData
     }
   }
 
