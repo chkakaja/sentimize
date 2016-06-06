5fa838ad3e1841886266f588e3412ee5a857ed9c
Jack Zhang <15jzhang@gmail.com>
2016-06-06 11:31:23 -0700

    Add link to SettingsView

--- client/components/main-layout/NavBar.jsx
+++ client/components/main-layout/NavBar.jsx
@@ -17,7 +17,7 @@ export default class NavBar extends React.Component {
             <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
               <div className="pure-menu-link nav-bar-dropdown"><i className="fa fa-cog fa-lg" aria-hidden="true"></i></div>
               <ul className="pure-menu-children"> 
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link dropdown-link">Settings</a></li>
+                <li className="pure-menu-item"><Link to="/settings" className="pure-menu-link dropdown-link">Settings</Link></li>
                 <li className="pure-menu-item"><a href="/logout" className="pure-menu-link dropdown-link">Log out</a></li>
               </ul>
             </li>
