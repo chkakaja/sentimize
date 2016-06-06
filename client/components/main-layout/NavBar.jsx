62ab89f523e369ba0b3d53955e74f0c99e63b951
Jack Zhang <15jzhang@gmail.com>
2016-06-06 11:29:15 -0700

    Update Footer Links to open new tabs

--- client/components/main-layout/NavBar.jsx
+++ client/components/main-layout/NavBar.jsx
@@ -15,7 +15,7 @@ export default class NavBar extends React.Component {
             <li className="pure-menu-item"><Link to="/record" className="pure-menu-link">Record</Link></li>
             <li className="pure-menu-item"><Link to="/sessions" className="pure-menu-link">Sessions</Link></li>
             <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
-              <div className="pure-menu-link nav-bar-dropdown"><i className="fa fa-cog" aria-hidden="true"></i></div>
+              <div className="pure-menu-link nav-bar-dropdown"><i className="fa fa-cog fa-lg" aria-hidden="true"></i></div>
               <ul className="pure-menu-children"> 
                 <li className="pure-menu-item"><a href="#" className="pure-menu-link dropdown-link">Settings</a></li>
                 <li className="pure-menu-item"><a href="/logout" className="pure-menu-link dropdown-link">Log out</a></li>
