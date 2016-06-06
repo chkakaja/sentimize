b34b9b5438d22e1c7bb346f1ae2a6a91276104f7
Jack Zhang <15jzhang@gmail.com>
2016-06-06 14:42:03 -0700

    Add button feedback after successfully changing profile data

--- client/components/settings-view/SettingsView.jsx
+++ client/components/settings-view/SettingsView.jsx
@@ -43,7 +43,8 @@ export default class SettingsView extends React.Component {
       data: formData,
       success: function(data) {
         console.log(data);
-      },
+        $(this.refs.updateButton).text('Profile updated');
+      }.bind(this),
       error: function(err) {
         console.error('_updateUserProfile error', err);
       },
@@ -52,7 +53,24 @@ export default class SettingsView extends React.Component {
   }
 
   _setNewPassword() {
+    let formData = {
+      currentPassword: this.refs.currentPassword.value,
+      newPassword: this.refs.newPassword.value,
+      hasNewPassword: true
+    };
 
+    $.ajax({
+      method: 'PUT',
+      url: '/api/users',
+      data: formData,
+      success: function(data) {
+        console.log(data);
+      }.bind(this),
+      error: function(err) {
+        console.error('_setNewPassword error', err);
+      },
+      dataType: 'json'
+    });
   }
 
   render() {
@@ -76,20 +94,20 @@ export default class SettingsView extends React.Component {
             <input type="email" name="Email" placeholder="Update Email" ref="email" required></input>
           </div>
         </form>
-        <button className="update-button" onClick={this._updateProfile.bind(this)}>Update profile</button>
+        <button className="update-button" ref="updateButton" onClick={this._updateProfile.bind(this)}>Update profile</button>
 
         <h4 className="form-title">Password</h4>
         <form className="update-password-form">
           <div className="update-form-label">Current Password</div>
           <div className="update-form-input current-password">
-            <input type="password" name="currentpassword" placeholder="Enter current password" required></input>
+            <input type="password" name="currentpassword" placeholder="Enter current password" ref="currentPassword" required></input>
           </div>
           <div className="update-form-label">New Password</div>
           <div className="update-form-input new-password">
-            <input type="password" name="newpassword" placeholder="Enter new password" required></input>
+            <input type="password" name="newpassword" placeholder="Enter new password" ref="newPassword" required></input>
           </div>
         </form>
-        <button className="update-password-button">Set new password</button>
+        <button className="update-password-button" onClick={this._setNewPassword.bind(this)}>Set new password</button>
       </div>
     )
   }
