cf26e2886a1bcce73a4f74117a0c66fc0cde5f68
Katherine <katherine.sj@gmail.com>
2016-06-06 17:26:16 -0700

    fixed questions

--- client/components/record-view/record-questions.jsx
+++ client/components/record-view/record-questions.jsx
@@ -6,8 +6,9 @@ export default (props) => (
 	<div className="record-questions pure-u-1-1">
 		<h1>Questions</h1>
       <ul>
-	      <li>How do you like Sentimize?</li>
-	      <li>I'm in another component now</li>
+	      <li>Introduce yourself within 30 seconds.</li>
+	      <li>Tell us about a challenge you recently faced and how you resolved it.</li>
+	      <li>What are your strengths and weaknesses?</li>
      </ul>
      <div className="button-bar">
         <button className="stop-button pure-button pure-button-error" onClick={(e) => props.clicked(e)}>Stop</button>
